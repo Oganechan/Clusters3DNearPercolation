84ce9fc656ec7d3da0de7f84fd99b5feb42ff61c
oganetyan <timofeev.nikita12@yandex.ru>
2025-04-08 20:54:15 +1000

    added cacheability of json nodes

--- include/config/config.h
+++ include/config/config.h
@@ -23,24 +23,24 @@ public:
     template <typename T>
     T get(const std::string &key, const T &default_value = T()) const
     {
-        if (!is_loaded_)
-            throw std::runtime_error("Config not loaded");
-        try
-        {
-            json current = config_;
-            size_t pos = 0;
-            std::string k = key;
-            while ((pos = k.find('.')) != std::string::npos)
-            {
-                current = current.at(k.substr(0, pos));
-                k = k.substr(pos + 1);
-            }
-            return current.value(k, default_value);
-        }
-        catch (const json::exception &e)
-        {
-            throw std::runtime_error("Config error at '" + key + "': " + e.what());
-        }
+        // if (!is_loaded_)
+        //     throw std::runtime_error("Config not loaded");
+        // try
+        // {
+        //     json current = config_;
+        //     size_t pos = 0;
+        //     std::string k = key;
+        //     while ((pos = k.find('.')) != std::string::npos)
+        //     {
+        //         current = current.at(k.substr(0, pos));
+        //         k = k.substr(pos + 1);
+        //     }
+        //     return current.value(k, default_value);
+        // }
+        // catch (const json::exception &e)
+        // {
+        //     throw std::runtime_error("Config error at '" + key + "': " + e.what());
+        // }
     }
 
     int lattice_size() const { return get<int>("lattice.size"); }
@@ -56,6 +56,8 @@ private:
     void operator=(const Config &) = delete;
 
     json config_;
+    mutable std::unordered_map<std::string, json> path_cache_;
+    mutable std::mutex cache_mutex_;
     std::mutex load_mutex_;
     bool is_loaded_ = false;
 };
