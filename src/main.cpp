c840f2a510bf0663f7ce106db7e4bdf5ade9b3e2
oganetyan <timofeev.nikita12@yandex.ru>
2025-04-09 15:13:37 +1000

    added lambda function, changed output

--- src/main.cpp
+++ src/main.cpp
@@ -39,35 +39,37 @@ int main(int argc, char **argv)
 
     std::vector<long double> average_cluster_counts;
 
-    // Simple Cubic lattice simulation
-    if (lattice_type == "SC" || lattice_type == "ALL")
+    auto simulate_lattice = [&](const std::string &lattice_name)
     {
-        std::cout << "Lattice type is SC" << std::endl;
-        Lattice sc_lattice("SC");
-        for (double concentration = CFG.get<double>("simulation.initial_concentration");
-             concentration <= CFG.get<double>("simulation.final_concentration");
-             concentration += CFG.get<double>("simulation.concentration_step"))
+        std::cout << "Lattice type is " << lattice_name << "\n";
+
+        Lattice lattice(lattice_name);
+        double initial_concentration = CFG.get<double>("simulation.initial_concentration");
+        double final_concentration = CFG.get<double>("simulation.final_concentration");
+        double concentration_step = CFG.get<double>("simulation.concentration_step");
+        int num_configuration = CFG.get<int>("simulation.num_configurations");
+        int lattice_volume = CFG.lattice_volume();
+
+        for (double concentration = initial_concentration; concentration <= final_concentration; concentration += concentration_step)
         {
-            std::cout << "Current concentration: " << concentration << std::endl;
-            int non_magnetic_count = std::ceil((1 - concentration) * CFG.lattice_volume());
+            int non_magnetic_count = std::ceil((1 - concentration) * lattice_volume);
             long total_clusters = 0;
-            for (int configuration = 0; configuration < CFG.get<int>("simulation.num_configurations"); ++configuration)
+
+            for (int configuration = 0; configuration < num_configuration; ++configuration)
             {
-                std::cout << "\033[2K\rConfiguration: " << configuration + 1 << " of "
-                          << CFG.get<int>("simulation.num_configurations") << std::flush;
-                sc_lattice.initialize();
-                sc_lattice.replace_random_spins(non_magnetic_count);
-                auto clusters = sc_lattice.find_clusters();
-                int multi_atom_clusters_count = 0;
-                for (const auto &cluster : clusters)
-                    if (cluster.size() != 1)
-                        ++multi_atom_clusters_count;
-                total_clusters += multi_atom_clusters_count;
+                std::cout << "\rConcentration: " << concentration << "\t|\tConfig:" << configuration + 1 << "/" << num_configuration << std::flush;
+
+                lattice.initialize();
+                lattice.replace_random_spins(non_magnetic_count);
+                auto clusters = lattice.find_clusters();
+
+                total_clusters += clusters.size();
             }
-            std::cout << std::endl;
-            average_cluster_counts.push_back(total_clusters / CFG.get<int>("simulation.num_configurations"));
+            std::cout << "\n";
+            average_cluster_counts.push_back(total_clusters / num_configuration);
         }
-        std::ofstream sc_output_file("../data/output/clusters_sc.txt");
+
+        std::ofstream sc_output_file("../data/output/clusters_" + lattice_name + ".txt");
         if (sc_output_file.is_open())
         {
             for (const auto &avg_count : average_cluster_counts)
@@ -78,92 +80,15 @@ int main(int argc, char **argv)
         else
             std::cerr << "Failed to open file" << std::endl;
         std::cout << std::endl;
-    }
+        average_cluster_counts.clear();
+    };
 
-    average_cluster_counts.clear();
-
-    // Body-Centered Cubic lattice simulation
+    if (lattice_type == "SC" || lattice_type == "ALL")
+        simulate_lattice("SC");
     if (lattice_type == "BCC" || lattice_type == "ALL")
-    {
-        std::cout << "Lattice type is BCC" << std::endl;
-        Lattice bcc_lattice("BCC");
-        for (double concentration = CFG.get<double>("simulation.initial_concentration");
-             concentration <= CFG.get<double>("simulation.final_concentration");
-             concentration += CFG.get<double>("simulation.concentration_step"))
-        {
-            std::cout << "Current concentration: " << concentration << std::endl;
-            int non_magnetic_count = std::ceil((1 - concentration) * CFG.lattice_volume());
-            long total_clusters = 0;
-            for (int configuration = 0; configuration < CFG.get<int>("simulation.num_configurations"); ++configuration)
-            {
-                std::cout << "\033[2K\rConfiguration: " << configuration + 1 << " of "
-                          << CFG.get<int>("simulation.num_configurations") << std::flush;
-                bcc_lattice.initialize();
-                bcc_lattice.replace_random_spins(non_magnetic_count);
-                auto clusters = bcc_lattice.find_clusters();
-                int multi_atom_clusters_count = 0;
-                for (const auto &cluster : clusters)
-                    if (cluster.size() != 1)
-                        ++multi_atom_clusters_count;
-                total_clusters += multi_atom_clusters_count;
-            }
-            std::cout << std::endl;
-            average_cluster_counts.push_back(total_clusters / CFG.get<int>("simulation.num_configurations"));
-        }
-        std::ofstream bcc_output_file("../data/output/clusters_bcc.txt");
-        if (bcc_output_file.is_open())
-        {
-            for (const auto &avg_count : average_cluster_counts)
-                bcc_output_file << avg_count << "\n";
-            bcc_output_file.close();
-            std::cout << "Data saved" << std::endl;
-        }
-        else
-            std::cerr << "Failed to open file" << std::endl;
-        std::cout << std::endl;
-    }
-
-    average_cluster_counts.clear();
-
-    // Face-Centered Cubic lattice simulation
+        simulate_lattice("BCC");
     if (lattice_type == "FCC" || lattice_type == "ALL")
-    {
-        std::cout << "Lattice type is FCC" << std::endl;
-        Lattice fcc_lattice("FCC");
-        for (double concentration = CFG.get<double>("simulation.initial_concentration");
-             concentration <= CFG.get<double>("simulation.final_concentration");
-             concentration += CFG.get<double>("simulation.concentration_step"))
-        {
-            std::cout << "Current concentration: " << concentration << std::endl;
-            int non_magnetic_count = std::ceil((1 - concentration) * CFG.lattice_volume());
-            long total_clusters = 0;
-            for (int configuration = 0; configuration < CFG.get<int>("simulation.num_configurations"); ++configuration)
-            {
-                std::cout << "\033[2K\rConfiguration: " << configuration + 1 << " of "
-                          << CFG.get<int>("simulation.num_configurations") << std::flush;
-                fcc_lattice.initialize();
-                fcc_lattice.replace_random_spins(non_magnetic_count);
-                auto clusters = fcc_lattice.find_clusters();
-                int multi_atom_clusters_count = 0;
-                for (const auto &cluster : clusters)
-                    if (cluster.size() != 1)
-                        ++multi_atom_clusters_count;
-                total_clusters += multi_atom_clusters_count;
-            }
-            std::cout << std::endl;
-            average_cluster_counts.push_back(total_clusters / CFG.get<int>("simulation.num_configurations"));
-        }
-        std::ofstream fcc_output_file("../data/output/clusters_fcc.txt");
-        if (fcc_output_file.is_open())
-        {
-            for (const auto &avg_count : average_cluster_counts)
-                fcc_output_file << avg_count << "\n";
-            fcc_output_file.close();
-            std::cout << "Data saved" << std::endl;
-        }
-        else
-            std::cerr << "Failed to open file" << std::endl;
-        std::cout << std::endl;
-    }
+        simulate_lattice("FCC");
+
     return 0;
 }
